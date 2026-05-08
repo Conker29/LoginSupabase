c0a322d774c51cc3277330731f4005ab4b46cc6a
Conker29 <pablo-e1@hotmail.com>
2026-05-08 15:19:16 -0500

    Update Supabase URL and key in environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://fmcvykibwgdkqowltnfk.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtY3Z5a2lid2dka3Fvd2x0bmZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzYwMDEsImV4cCI6MjA5Mzc1MjAwMX0.7vHDL2jaADXpWnKW_8_caIa2FalTUhCuSLgIW23q4Fg'
+  supabaseUrl: 'https://tu-proyecto.supabase.co',
+  supabaseKey: 'tu ANON KEY'
 };
